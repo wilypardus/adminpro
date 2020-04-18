6fe906c51d7a3e63a389fa6c262c2e0d04c02007
Will <guivalav@gmail.com>
2020-04-18 11:48:33 +0200

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,20 @@
+import { PagesComponent } from './pages.component';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+const pagesRoutes: Routes = [
+  {path: '',
+  component: PagesComponent,
+  children:[
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas1', component: Graficas1Component },
+      { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+  ]
+}
+];
+
+export const PAGES_ROUTES=RouterModule.forChild(pagesRoutes);
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ],
+  providers: [],
+})
+export class SharedModule {}
